4852bb43f40022b9869a91be750963a5aa2cccc9
Minghao Liu <922lmh922@gmail.com>
2015-05-15 14:03:25 +0800

    Finished gwt rpc.

--- src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
@@ -14,6 +15,7 @@ import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.user.client.Random;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -31,6 +33,8 @@ public class StockWatcher implements EntryPoint {
 	private Button addStockButton = new Button("Add");
 	private Label lastUpdatedLabel = new Label();
 	private ArrayList<String> stocks = new ArrayList<String>();
+	private StockPriceServiceAsync stockPriceSvc = GWT.create(StockPriceService.class);
+	private Label errorMsgLabel = new Label();
 
 	/**
 	 * Entry point method.
@@ -57,7 +61,11 @@ public class StockWatcher implements EntryPoint {
 	    addPanel.add(addStockButton);
 	    addPanel.addStyleName("addPanel");
 
-	    // Assemble Main panel.
+	 // Assemble Main panel.
+	    errorMsgLabel.setStyleName("errorMessage");
+	    errorMsgLabel.setVisible(false);
+
+	    mainPanel.add(errorMsgLabel);
 	    mainPanel.add(stocksFlexTable);
 	    mainPanel.add(addPanel);
 	    mainPanel.add(lastUpdatedLabel);
@@ -88,21 +96,32 @@ public class StockWatcher implements EntryPoint {
 	      });
 	}
 
-	protected void refreshWatchList() {
-		// TODO Auto-generated method stub
-		 final double MAX_PRICE = 100.0; // $100.00
-	     final double MAX_PRICE_CHANGE = 0.02; // +/- 2%
+	private void refreshWatchList() {
+	    // Initialize the service proxy.
+	    if (stockPriceSvc == null) {
+	      stockPriceSvc = GWT.create(StockPriceService.class);
+	    }
 
-	     StockPrice[] prices = new StockPrice[stocks.size()];
-	     for (int i = 0; i < stocks.size(); i++) {
-	       double price = Random.nextDouble() * MAX_PRICE;
-	       double change = price * MAX_PRICE_CHANGE
-	           * (Random.nextDouble() * 2.0 - 1.0);
+	     // Set up the callback object.
+	    AsyncCallback<StockPrice[]> callback = new AsyncCallback<StockPrice[]>() {
+	    	public void onFailure(Throwable caught) {
+	    	    // If the stock code is in the list of delisted codes, display an error message.
+	    	    String details = caught.getMessage();
+	    	    if (caught instanceof DelistedException) {
+	    	      details = "Company '" + ((DelistedException)caught).getSymbol() + "' was delisted";
+	    	    }
 
-	       prices[i] = new StockPrice(stocks.get(i), price, change);
-	     }
+	    	    errorMsgLabel.setText("Error: " + details);
+	    	    errorMsgLabel.setVisible(true);
+	    	}
 
-	     updateTable(prices);
+	      public void onSuccess(StockPrice[] result) {
+	        updateTable(result);
+	      }
+	    };
+
+	     // Make the call to the stock price service.
+	    stockPriceSvc.getPrices(stocks.toArray(new String[0]), callback);
 	}
 
 	 /**
@@ -120,6 +139,8 @@ public class StockWatcher implements EntryPoint {
         DateTimeFormat.PredefinedFormat.DATE_TIME_MEDIUM);
       lastUpdatedLabel.setText("Last update : "
         + dateFormat.format(new Date()));
+   // Clear any errors.
+      errorMsgLabel.setVisible(false);
     }
 
     /**
